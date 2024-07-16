1c35adf41229ca55f9d695d2ffc3a90e6ee95601
Aman Raj <135144595+Amanraj0149@users.noreply.github.com>
2024-07-17 01:17:59 +0530

    first commit

---
+++ src/components/Credit.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Credit = () => {
+  const [theme, setTheme] = React.useState("night");
+  const changeTheme = () => {
+    const themes = [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+    ];
+    let theme = themes[Math.floor(Math.random() * themes.length)];
+    setTheme(theme);
+    document.querySelector("html").dataset.theme = theme;
+  };
+  return (
+    <p className="absolute bottom-4 right-4 flex flex-col gap-2">
+      <button onClick={changeTheme} className="btn">
+        {theme}
+      </button>
+    </p>
+  );
+};
+
+export default Credit;
